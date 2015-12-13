12abc345aeeeb69605bb524709e729fad1e09dfb
Janik Knittle <janik.knittle@gmail.com>
2015-12-13 12:27:05 -0800

    Create some brand and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+janik = User.create! :username => 'Janik', :email => 'janik.knittle@gmail.com', :password => 'password', :password_confirmation => 'password'
+
+brands = ["Acerbis", "Hinson", "Tusk Racing", "ARC", "Barnett", "Yamaha", "Kawasaki", "KTM", "Beta", "FORD", "Chevrolet", "Husqvarna"]
+
+brands.each do |name| 
+  Brand.create(name: name)
+end
