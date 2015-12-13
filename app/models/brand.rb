ed00d8039a9ba91e17dd6417ca31593e59fa3851
Janik Knittle <janik.knittle@gmail.com>
2015-12-13 12:17:33 -0800

    Add basic validation to brand name

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,5 @@
 class Brand < ActiveRecord::Base
+
+  validates :name, uniqueness: { case_sensitive: false, message: "brand already exists" }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213195000) do
+ActiveRecord::Schema.define(version: 20151213201318) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.string   "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "manufacturers", force: :cascade do |t|
     t.string   "name"
