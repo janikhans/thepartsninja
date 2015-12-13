97f102a33acf04064d5c0875b769a4a48e624168
Janik Knittle <janik.knittle@gmail.com>
2015-12-12 17:40:28 -0800

    Setup username fields and allow to sign in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, uniqueness: { case_sensitive: false}, presence: true, length: { in: 4..20}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validate :validate_username
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+  
 end
