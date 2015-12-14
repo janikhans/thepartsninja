82fa6cffd41817b11187984f4c89235ec1751d78
Janik Knittle <janik.knittle@gmail.com>
2015-12-13 22:29:57 -0800

    saved old form files

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand < ActiveRecord::Base
 
   has_many :vehicles
-  validates :name, uniqueness: { case_sensitive: false, message: "brand already exists" }
-
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: "brand already exists" }
+  
 end
