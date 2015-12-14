2e9fc2d24629b2d65ff3e6eec3f8def2cac18d68
Janik Knittle <janik.knittle@gmail.com>
2015-12-13 23:21:57 -0800

    Error messages and made some simple validations

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,14 @@
 class Brand < ActiveRecord::Base
 
   has_many :vehicles
+  before_validation :strip_and_upcase_name
   validates :name, presence: true, uniqueness: { case_sensitive: false, message: "brand already exists" }
-  
+
+  private
+
+    def strip_and_upcase_name
+      self.name = name.strip
+      self.name = name[0].upcase + name[1..-1]
+    end
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,17 +1,29 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :brand
 
+  before_validation :strip_and_upcase_model
   validates :model, :brand, presence: true
-  validates :year, presence: true, numericality: true, :inclusion => { in: 1900..Date.today.year+1, message: "needs to be between 1900-#{Date.today.year+1}"}
-  validates :year, uniqueness: {scope: [:brand_id, :model], message: "This model year already exists"}
+  validates :year, presence: true, 
+                   numericality: true, 
+                   inclusion: { in: 1900..Date.today.year+1, message: "needs to be between 1900-#{Date.today.year+1}"}, 
+                   uniqueness: {scope: [:brand_id, :model], message: "This model year already exists"}
 
-
+  #Needs to strip and upcase before searching to be sure
   def brand_name
     brand.try(:name)
   end
 
+  #This one too
   def brand_name=(name)
     self.brand = Brand.find_or_create_by(name: name) if name.present?
   end
+
+
+private
+  #This and the strip_and_upcase_name in brand.rb can be DRY'd up at some point
+    def strip_and_upcase_model
+      self.model = model.strip
+      self.model = model[0].upcase + model[1..-1]
+    end
 
 end
