8b245f09dd8b9be740a711770fadaff2327d8afd
Janik Knittle <janik.knittle@gmail.com>
2015-12-31 14:00:05 -0800

    Discovery form works basically without backwards compatible option - not pretty at all

--- app/controllers/discoveries_controller.rb
+++ app/controllers/discoveries_controller.rb
@@ -29,10 +29,43 @@ class DiscoveriesController < ApplicationController
   # POST /discoveries
   # POST /discoveries.json
   def create
+
+    #find OEM Vehicle
+    og_brand_name = params[:og_vehicle][:brand].strip
+    og_brand = Brand.where('lower(name) = ?', og_brand_name.downcase).first_or_create(name: og_brand_name)
+    og_model = params[:og_vehicle][:model].strip
+    og_year = params[:og_vehicle][:year]
+    og_vehicle = Vehicle.where(brand: og_brand).where(year: og_year).where('lower(model) = ?', og_model.downcase).first_or_create(brand: og_brand, year: og_year, model: og_model)
+
+    og_part_brand_name = params[:og_part][:brand].strip
+    og_part_brand = Brand.where('lower(name) = ?', og_part_brand_name.downcase).first_or_create(name: og_part_brand_name)
+    og_part_name = params[:og_part][:name].strip
+    og_product = Product.where(brand: og_part_brand).where('lower(name) = ?', og_part_name.downcase).first_or_create(brand: og_part_brand, name: og_part_name)
+
+    compat_brand_name = params[:compat_vehicle][:brand].strip
+    compat_brand = Brand.where('lower(name) = ?', compat_brand_name.downcase).first_or_create(name: compat_brand_name)
+    compat_model = params[:compat_vehicle][:model].strip
+    compat_year = params[:compat_vehicle][:year]
+    compat_vehicle = Vehicle.where(brand: compat_brand).where(year: compat_year).where('lower(model) = ?', compat_model.downcase).first_or_create(brand: compat_brand, year: compat_year, model: compat_model)
+   
+    compat_part_brand_name = params[:compat_part][:brand].strip
+    compat_part_brand = Brand.where('lower(name) = ?', compat_part_brand_name.downcase).first_or_create(name: compat_part_brand_name)
+    compat_part_name = params[:compat_part][:name].strip
+    compat_product = Product.where(brand: compat_part_brand).where('lower(name) = ?', compat_part_name.downcase).first_or_create(brand: compat_part_brand, name: compat_part_name) 
+
+
+    og_fitment_part = og_vehicle.oem_parts.where(product: og_product).first_or_create(product: og_product)
+    og_fitment = og_vehicle.fitments.where(part: og_fitment_part).first
+    compat_fitment_part = compat_vehicle.oem_parts.where(product: compat_product).first_or_create(product: compat_product)
+    compat_fitment = compat_vehicle.fitments.where(part: compat_fitment_part).first
+
     @discovery = current_user.discoveries.build(discovery_params)
+    @compatible = @discovery.compatibles.build
+    @compatible.fitment = og_fitment
+    @compatible.compatible_fitment = compat_fitment
 
     respond_to do |format|
-      if @discovery.save
+      if @discovery.save && @compatible.save
         format.html { redirect_to @discovery, notice: 'Discovery was successfully created.' }
         format.json { render :show, status: :created, location: @discovery }
       else
--- app/models/part.rb
+++ app/models/part.rb
@@ -3,8 +3,17 @@ class Part < ActiveRecord::Base
   belongs_to :user
   has_many :fitments, dependent: :destroy
   has_many :oem_vehicles, through: :fitments, source: :vehicle
-  has_many :potential_vehicles, through: :fitments, source: :known_compatibles
-  has_many :compatible_vehicles, through: :fitments, source: :potential_compatibles
+
+  has_many :known_compatibles, through: :fitments
+  has_many :known_not_backwards_compatibles, through: :fitments
+  has_many :backwards_compatibles, through: :fitments
+  has_many :potential_compatibles, through: :fitments
+
+  def compatible_vehicles
+    known_compatibles | known_not_backwards_compatibles
+  end
+
 
   validates :product, presence: true
 end
+
