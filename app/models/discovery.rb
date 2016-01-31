2d2f2a87fc57ca7bb8a55582dd09c78953e5ba74
Janik Knittle <janik.knittle@gmail.com>
2016-01-31 10:35:51 -0800

    Fixed issue with New Discovery form

--- app/models/discovery.rb
+++ app/models/discovery.rb
@@ -8,17 +8,17 @@ class Discovery < ActiveRecord::Base
   accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
 
   #Work around so that the discovery form validates and saves info in case it's wrong
-  # attr_accessor :oem_part_brand
-  # attr_accessor :oem_part_name
-  # attr_accessor :oem_vehicle_brand
-  # attr_accessor :oem_vehicle_year
-  # attr_accessor :oem_vehicle_model
-  # attr_accessor :compatible_vehicle_brand
-  # attr_accessor :compatible_vehicle_year
-  # attr_accessor :compatible_vehicle_model
-  # attr_accessor :compatible_part_name
-  # attr_accessor :compatible_part_brand
-  # attr_accessor :backwards
+  attr_accessor :oem_part_brand
+  attr_accessor :oem_part_name
+  attr_accessor :oem_vehicle_brand
+  attr_accessor :oem_vehicle_year
+  attr_accessor :oem_vehicle_model
+  attr_accessor :compatible_vehicle_brand
+  attr_accessor :compatible_vehicle_year
+  attr_accessor :compatible_vehicle_model
+  attr_accessor :compatible_part_name
+  attr_accessor :compatible_part_brand
+  attr_accessor :backwards
 
   # validates :oem_part_brand, :oem_part_name, :compatible_vehicle_brand, :compatible_vehicle_model, :compatible_part_name, :compatible_part_brand, presence: true
   # validates :compatible_vehicle_year, presence: true, numericality: true #, inclusion: { in: 1900..Date.today.year+1, message: "needs to be between 1900-#{Date.today.year+1}"}
