32b4a0440bc6e257d94df50a4d87c74eb6632139
Janik Knittle <janik.knittle@gmail.com>
2016-02-01 12:21:15 -0800

    Change Fitment migration to reflect parts now

--- db/migrate/20151220220650_create_compatibles.rb
+++ db/migrate/20151220220650_create_compatibles.rb
@@ -1,8 +1,8 @@
 class CreateCompatibles < ActiveRecord::Migration
   def change
     create_table :compatibles do |t|
-      t.integer :fitment_id, index: true, foreign_key: true
-      t.integer :compatible_fitment_id, index: true, foreign_key: true
+      t.integer :part_id, index: true, foreign_key: true
+      t.integer :compatible_part_id, index: true, foreign_key: true
       t.integer :discovery_id, index: true, foreign_key: true
       t.boolean :backwards, default: false, null: false
 
