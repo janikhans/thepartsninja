9624d7ae86705b241e2f201d9341df26ad11abf4
Janik Knittle <janik.knittle@gmail.com>
2016-04-18 01:38:09 -0700

    Total change over to dark styling

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -20,7 +20,7 @@ $(function() {
         var docElem = document.documentElement,
                 header = document.querySelector( '.navbar-default' ),
                 didScroll = false,
-                changeHeaderOn = 200;
+                changeHeaderOn = 25;
         function init() {
             window.addEventListener( 'scroll', function( event ) {
                 if( !didScroll ) {
