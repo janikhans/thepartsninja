566968594f32bcb59b5f2c260e5e9e94d4f6d7be
Janik Knittle <janik.knittle@gmail.com>
2016-04-26 15:42:34 -0700

    Fixed header styling

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@partsninja.io'
+  config.mailer_sender = 'noreply@theparts.ninja'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
