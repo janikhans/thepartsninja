32a99df64a6abdcb3cb5fc5b93a1341a9dee5eab
Janik Knittle <janik.knittle@gmail.com>
2016-08-11 11:19:59 -0700

    Fix a few old method issues

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,6 +3,8 @@ class Brand < ActiveRecord::Base
   # vehicle manufacturer, part manufacturer and/or both
   # Potentially an ebay ID
   # potentially a user_id if it's added by a user
+  # attribute for brand description, social media sites?
+  # let a brand create an account and get special access?
 
   extend FriendlyId
   friendly_id :name, use: [:finders, :slugged]
@@ -10,6 +12,9 @@ class Brand < ActiveRecord::Base
   has_many :vehicle_models,
     inverse_of: :brand,
     dependent: :destroy
+
+  has_many :vehicles,
+    through: :vehicle_models
 
   has_many :products,
     dependent: :destroy
