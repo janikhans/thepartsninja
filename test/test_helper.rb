8bdce7827085429fa1c7da490c774b3a5f8decf3
Janik Knittle <janik.knittle@gmail.com>
2016-08-15 14:01:07 -0700

    Remove old unused files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,31 +18,6 @@ end
 
 class UnitTest < ActiveSupport::TestCase
   fixtures :all
-  # def self.assert_all_fixtures_valid(klass)
-  #   invalids = klass.all.reject { |k| k.valid? }
-  #   assert invalids.empty?, "The following are invalid:\n\t #{invalids.join("\n\t")}"
-  # end
-
-  def self.find_all_invalid_fixtures
-    fixtures = Dir.glob("test/fixtures/*.yml")
-    fixtures.map! { |f| File.basename f, ".yml"}
-    fixtures.map! { |f| f.classify.constantize }
-
-    fixtures.each do |f|
-      assert_all_fixtures_valid(f)
-    end
-  end
-
-  def self.assert_all_fixtures_valid(klass)
-    invalids = klass.all.reject { |k| k.valid? }
-    unless invalids.empty?
-      puts "\n--------------------\nThe following #{klass} fixtures are invalid:\n\n"
-      invalids.each do |i|
-        puts "\tid: #{i.id}, #{i.errors.full_messages}"
-      end
-      # puts "\n--------------------"
-    end
-  end
 
   def assert_differences(expression_array, message = nil, &block)
     b = block.send(:binding)
