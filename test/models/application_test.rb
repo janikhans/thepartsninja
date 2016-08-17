826eeebd27dd3ea687b95059ef4cea7153ff24f2
Janik Knittle <janik.knittle@gmail.com>
2016-08-16 22:14:13 -0700

    Update application_test

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,45 +1,39 @@
-require "test_helper"
+require 'test_helper'
 
-class ApplicationTest < UnitTest
+class ApplicationTest < ActiveSupport::TestCase
+
   test "all models should inherit from ApplicationRecord" do
-    application_record = ApplicationRecord.descendants
-    active_record = ActiveRecord::Base.descendants
+    application_records = ApplicationRecord.descendants
+    active_records = ActiveRecord::Base.descendants
 
     blacklist = [ApplicationRecord, ActiveRecord::SchemaMigration]
-    uninherited = active_record.reject { |a| application_record.include?(a) || blacklist.include?(a) }
+    uninherited = active_records - application_records - blacklist
 
     assert uninherited.empty?,
-           "The following models do not inherit from ApplicationRecord:\n\t#{uninherited.join("\n\t")}"
+      "The following models do not inherit from ApplicationRecord:\n\t#{uninherited.join("\n\t")}"
   end
 
   test "all fixtures should be valid" do
     invalids = find_all_invalid_fixtures
-    assert invalids.empty?,
-           "The following are invalid:\n\t#{invalids.join("\n\t")}"
+    assert invalids.empty?, "The following fixtures are invalid:\n\t#{invalids.join("\n\t")}"
   end
 
   def find_all_invalid_fixtures
-    fixtures = Dir.glob("test/fixtures/*.yml")
-    fixtures.map! { |f| File.basename f, ".yml" }
-    fixtures.map! { |f| f.classify.constantize }
-
-    invalids = []
-    fixtures.each do |f|
-      invalids << find_invalid_fixtures_for(f)
-    end
-
-    invalids.flatten!
+    fixture_classes = Dir.glob("test/fixtures/*.yml")
+      .map { |f| File.basename f, ".yml" }
+      .map { |f| f.classify.constantize }
+      .reduce([]) { |invalids, f| invalids << find_invalid_fixtures_for(f) }
+      .flatten
   end
 
   def find_invalid_fixtures_for(klass)
     invalid_fixtures = klass.all.reject(&:valid?)
-    offending_fixtures = []
-    unless invalid_fixtures.empty?
-      invalid_fixtures.each do |i|
-        attributes = i.attributes.symbolize_keys.except!(:id, :created_at, :updated_at)
-        offending_fixtures << "[#{i.class.name}, #{attributes}, #{i.errors.full_messages}]"
-      end
+    invalid_fixtures.map do |i|
+      "#{i.class.name} with errors \"#{i.errors.full_messages.join(", ")}\" and attributes #{attributes_for(i)}"
     end
-    offending_fixtures
+  end
+
+  def attributes_for(fixture)
+    fixture.attributes.symbolize_keys.except!(:id, :created_at, :updated_at)
   end
 end
