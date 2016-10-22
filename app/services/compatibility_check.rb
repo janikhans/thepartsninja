b8693f2f467bf7d1f9bf933473a85af635be78cb
Janik Knittle <janik.knittle@gmail.com>
2016-10-22 14:14:24 -0700

    Change Compatibility Check to use Part Attributes

--- app/services/compatibility_check.rb
+++ app/services/compatibility_check.rb
@@ -6,7 +6,7 @@ class CompatibilityCheck
     @vehicle_two = Vehicle.find_by(id: params[:vehicle_two_id])
     @product_type = ProductType.find_by(id: params[:product_type_id])
     # FIXME temp hack because rails form sends an empty param through the form
-    @part_attributes = params[:part_attributes].delete_if { |x| x.empty? }.first
+    @part_attributes = params[:part_attributes].delete_if { |x| x.empty? }
   end
 
   def process
@@ -14,8 +14,8 @@ class CompatibilityCheck
       vehicle_one_parts = @vehicle_one.oem_parts.joins(:product).where('products.product_type_id = ?', @product_type.id).includes(product: :brand)
       vehicle_two_parts = @vehicle_two.oem_parts.joins(:product).where('products.product_type_id = ?', @product_type.id).includes(product: :brand)
     else
-      vehicle_one_parts = @vehicle_one.oem_parts.joins(:product, :part_attributes).where('products.product_type_id = ? AND part_attributes.id = ?', @product_type.id, @part_attributes).includes(product: :brand)
-      vehicle_two_parts = @vehicle_two.oem_parts.joins(:product, :part_attributes).where('products.product_type_id = ? AND part_attributes.id = ?', @product_type.id, @part_attributes).includes(product: :brand)
+      vehicle_one_parts = @vehicle_one.oem_parts.joins(:product, :part_attributes).where('products.product_type_id = ? AND part_attributes.id IN (?)', @product_type.id, @part_attributes).includes(product: :brand)
+      vehicle_two_parts = @vehicle_two.oem_parts.joins(:product, :part_attributes).where('products.product_type_id = ? AND part_attributes.id IN (?)', @product_type.id, @part_attributes).includes(product: :brand)
     end
     @results = vehicle_one_parts & vehicle_two_parts
   end
