67afe176bcc0c322fc21a618a10846b2fcd0deba
janikhans <janik.knittle@gmail.com>
2017-02-26 19:17:12 -0800

    add header to find compatibilities search

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -162,3 +162,11 @@ window.utils = {
     });
   }
 };
+
+$(function() {
+  return $('.chosen-select').chosen({
+    allow_single_deselect: true,
+    no_results_text: 'No results matched',
+    width: '100%'
+  });
+});
