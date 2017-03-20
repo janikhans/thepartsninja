09ec1d3a513f8a6c21bc2f55eb2960c0a9e23d12
Janik Knittle <janik.knittle@gmail.com>
2017-03-19 21:04:45 -0700

    Remove unneccesary files

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -1,11 +1,6 @@
 $(function () {
   navbar.initialize();
 
-  pricing_charts.initialize();
-
-  global_notifications.initialize();
-
-
   retina.initialize();
 
   $('#sticky-sidebar').affix({
@@ -26,8 +21,6 @@ $(function () {
 
 var navbar = {
   initialize: function () {
-    this.themeSites();
-
     if (!window.utils.isMobile()) {
       this.dropdowns();
     }
@@ -44,64 +37,6 @@ var navbar = {
           $item.removeClass('open')
       })
     })
-  },
-
-  themeSites: function () {
-    var $trigger = $("[data-toggle-header-sections-menu]"),
-      $header_panel = $("#header-sections-menu"),
-      $body = $("body");
-
-    $trigger.click(function (e) {
-      e.preventDefault();
-
-      $body.toggleClass("header-panel-visible");
-
-      if ($body.hasClass("header-panel-visible")) {
-        $trigger.find("i").removeClass("ion-plus").addClass("ion-minus");
-
-        var $navbar = $trigger.closest(".navbar");
-
-        if ($navbar.hasClass("navbar-transparent")) {
-          $header_panel.removeClass("fixed");
-          return;
-        }
-
-        if ($navbar.hasClass("navbar-fixed-top")) {
-          $header_panel.addClass("fixed");
-        }
-      } else {
-        $trigger.find("i").removeClass("ion-minus").addClass("ion-plus");
-      }
-    })
-  }
-};
-
-var global_notifications = {
-  initialize: function () {
-    setTimeout(function () {
-      $(".global-notification").removeClass("uber-notification").addClass("uber-notification-remove");
-    }, 5000);
-  }
-};
-
-var pricing_charts = {
-  initialize: function () {
-    var tabs = $(".pricing-charts-tabs .tab"),
-        prices = $(".pricing-charts .chart header .price");
-
-    tabs.click(function () {
-      tabs.removeClass("active");
-      $(this).addClass("active");
-
-      var period = $(this).data("tab");
-      var price_out = prices.filter(":not(."+ period +")");
-      price_out.addClass("go-out");
-      prices.filter("." + period + "").addClass("active");
-
-      setTimeout(function () {
-        price_out.removeClass("go-out").removeClass("active");
-      }, 250);
-    });
   }
 };
 
@@ -149,31 +84,6 @@ window.utils = {
     } else {
       return false
     }
-  },
-
-  parallax_text: function ($selector, extra_top) {
-    extra_top = typeof extra_top !== 'undefined' ? extra_top : 0;
-
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop(),
-        slowScroll = scroll/1.4,
-        slowBg = (extra_top + slowScroll) + "px",
-        opacity,
-        transform = "transform" in document.body.style ? "transform" : "-webkit-transform";;
-
-      if (scroll > 0) {
-        opacity = (1000 - (scroll*2.7)) / 1000;
-      } else {
-        opacity = 1;
-      }
-
-      $selector.css({
-        "position": "relative",
-        // transform: "translate3d(0, " + slowBg + ", 0)",
-        top: slowBg,
-        "opacity": opacity
-      });
-    });
   }
 };
 
