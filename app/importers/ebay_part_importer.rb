2c8e1fa7ab98d171359b94c29e833d3544dc403c
Janik Knittle <janik.knittle@gmail.com>
2017-04-25 21:50:06 -0700

    Create ebay_part_importer and form object

---
+++ app/importers/ebay_part_importer.rb
@@ -0,0 +1,69 @@
+class EbayPartImporter < CSVParty
+  column :epid, header: 'ePID', as: :integer
+  column :product_brand, header: 'Part Brand', as: :pretty_string
+  column :product_name, header: 'Part Type', as: :pretty_string
+  column :part_number, header: 'Part number', as: :string
+  column :reserved_title, header: 'RESERVED_PRODUCT_TITLE', as: :string
+  column :category_id, header: 'Category ID'
+  column :category_breadcrumb, header: 'Category Breadcrumb', as: :string
+
+  import do |row|
+    next if Part.find_by(epid: row.epid)
+
+    begin
+      ActiveRecord::Base.transaction do
+        categories = row.category_breadcrumb.split(':')
+        root_category = categories[2]
+        category = categories[3]
+        subcategory = categories[4]
+
+        product_form = EbayProductForm.new(
+          brand: row.product_brand,
+          product_name: row.product_name,
+          root_category: root_category,
+          category: category,
+          subcategory: subcategory
+        )
+
+        unless product_form.save
+          raise ActiveModel::ValidationError, product_form
+        end
+
+        product = product_form.product
+        note = row.reserved_title.split(',')[1]
+
+        part = product.parts
+                      .where('lower(part_number) = ?', row.part_number.downcase)
+                      .first_or_initialize(part_number: row.part_number)
+        part.epid = row.epid
+        part.note = note
+        part.save!
+
+        if note.present?
+          attribute_list = note.split(' - ').map(&:strip)
+          attributes = PartAttribute.specific_attributes
+                                    .where(name: attribute_list)
+          attributes.each do |attribute|
+            attribute.part_attributions.where(part: part).first_or_create
+          end
+        end
+      end
+    rescue ActiveRecord::RecordInvalid, ActiveModel::ValidationError => e
+      EbayPartImportError.create(row: row.csv_string, import_errors: e.message)
+    end
+  end
+
+  def import!
+    super
+  rescue CSV::MalformedCSVError => e
+    row_num = $INPUT_LINE_NUMBER
+    error_row = IO.readlines(@csv.path)[row_num - 1]
+    EbayPartImportError.create(row: error_row, import_errors: e.message)
+  end
+
+  def pretty_string_parser(value)
+    return if value.blank?
+    value = value.strip
+    value[0].upcase + value[1..-1]
+  end
+end
--- test/forms/product_form_test.rb
+++ test/forms/product_form_test.rb
@@ -5,36 +5,36 @@ class ProductFormTest < UnitTest
   should validate_presence_of(:product_name)
   should validate_presence_of(:root_category)
   should validate_presence_of(:category)
-  should validate_length_of(:brand).is_at_most(75)
-  should validate_length_of(:product_name).is_at_most(75)
-  should validate_length_of(:category).is_at_most(75)
+  should validate_length_of(:brand).is_at_most(100)
+  should validate_length_of(:product_name).is_at_most(100)
+  should validate_length_of(:category).is_at_most(100)
 
   setup do
-    @product = ProductForm.new(brand: "Acerbis",
-                               product_name: "SXS Skidplate",
-                               root_category: "Motorcycle Parts",
-                               category: "Body",
-                               subcategory: "Skidplate")
+    @product = ProductForm.new(brand: 'Acerbis',
+                               product_name: 'SXS Skidplate',
+                               root_category: 'Motorcycle Parts',
+                               category: 'Body',
+                               subcategory: 'Skidplate')
   end
 
-  test "should sanitize fields" do
-    product = ProductForm.new(brand: "  Kawasaki  ",
-                        product_name: "   big skidplate  ",
-                        root_category: "  motorcycle Parts  ",
-                        category: "body",
-                        subcategory: "   sKidplatE   ")
+  test 'should sanitize fields' do
+    product = ProductForm.new(brand: '  Kawasaki  ',
+                              product_name: '   big skidplate  ',
+                              root_category: '  motorcycle Parts  ',
+                              category: 'body',
+                              subcategory: '   sKidplatE   ')
     assert product.valid?
 
-    assert_equal product.brand, "Kawasaki"
-    assert_equal product.product_name, "Big skidplate"
-    assert_equal product.root_category, "Motorcycle Parts"
-    assert_equal product.category, "Body"
-    assert_equal product.subcategory, "SKidplatE"
+    assert_equal product.brand, 'Kawasaki'
+    assert_equal product.product_name, 'Big skidplate'
+    assert_equal product.root_category, 'Motorcycle Parts'
+    assert_equal product.category, 'Body'
+    assert_equal product.subcategory, 'SKidplatE'
   end
 
-  test "product form should find and set brand if it exist" do
+  test 'product form should find and set brand if it exist' do
     product = @product
-    product.brand = "Yamaha"
+    product.brand = 'Yamaha'
     assert_equal product.brand, brands(:yamaha).name
     assert product.valid?
 
@@ -52,10 +52,10 @@ class ProductFormTest < UnitTest
       product.save
     end
 
-    assert_equal Brand.last.name, "Acerbis"
+    assert_equal Brand.last.name, 'Acerbis'
   end
 
-  test "should create product with only root_category and category" do
+  test 'should create product with only root_category and category' do
     product = @product
     product.subcategory = nil
     assert product.valid?
@@ -65,18 +65,19 @@ class ProductFormTest < UnitTest
     end
   end
 
-  test "product form should select existing parent, category AND subcategory if they exist" do
+  test 'product form should select existing parent, category AND subcategory if
+    they exist' do
     product = @product
-    product.root_category = "Motorcycle Parts"
-    product.category = "Bearings"
-    product.subcategory = "Wheel Bearings"
+    product.root_category = 'Motorcycle Parts'
+    product.category = 'Bearings'
+    product.subcategory = 'Wheel Bearings'
     root_category = Category.roots.where(name: product.root_category).first
     category = root_category.children.where(name: product.category).first
     subcategory = category.children.where(name: product.subcategory).first
     assert_not_nil root_category
     assert_not_nil category
     assert_not_nil subcategory
-    assert_includes category.children.map { |s| s.name }, product.subcategory
+    assert_includes category.children.map(&:name), product.subcategory
 
     assert product.valid?
     assert_no_difference ['Category.count'] do
@@ -84,9 +85,10 @@ class ProductFormTest < UnitTest
     end
   end
 
-  test "product form should find category and create subcategory if it doesn't exist" do
+  test 'product form should find category and create subcategory if it does not
+    exist' do
     product = @product
-    root_category = Category.where(name: product.root_category, parent_id: nil).first
+    root_category = Category.roots.where(name: product.root_category).first
     body_category = root_category.children.where(name: product.category).first
     assert_not_nil body_category
     assert_empty body_category.children
@@ -100,10 +102,11 @@ class ProductFormTest < UnitTest
     assert_not_empty body_category.children
   end
 
-  test "product form should create category and subcategory if both don't exist" do
+  test 'product form should create category and subcategory if both do not
+    exist' do
     product = @product
-    product.category = "New Category"
-    product.subcategory = "New Subcategory"
+    product.category = 'New Category'
+    product.subcategory = 'New Subcategory'
 
     assert_nil Category.where(name: product.category).first
 
@@ -117,17 +120,19 @@ class ProductFormTest < UnitTest
     assert_not_nil new_category
     assert_not_empty new_category.children
 
-    assert_equal new_category.children.first.name, "New Subcategory"
+    assert_equal new_category.children.first.name, 'New Subcategory'
   end
 
-  test "should find product if it already exists" do
-    product = ProductForm.new(brand: "Tusk Racing",
-                        product_name: "Sealed ball bearings",
-                        root_category: "Motorcycle Parts",
-                        category: "Bearings",
-                        subcategory: "Wheel Bearings")
+  test 'should find product if it already exists' do
+    product = ProductForm.new(brand: 'Tusk Racing',
+                              product_name: 'Sealed ball bearings',
+                              root_category: 'Motorcycle Parts',
+                              category: 'Bearings',
+                              subcategory: 'Wheel Bearings')
 
-    assert_not_nil Product.where(name: product.product_name, brand: Brand.where(name: product.brand).first).first
+    assert_not_nil Product.where(name: product.product_name,
+                                 brand: Brand.where(name: product.brand).first)
+      .first
     assert product.valid?
 
     assert_no_difference ['Product.count'] do
@@ -135,7 +140,7 @@ class ProductFormTest < UnitTest
     end
   end
 
-  test "should create new product if it doesn't already exist" do
+  test 'should create new product if it does not already exist' do
     product = @product
     assert product.valid?
 
@@ -146,23 +151,27 @@ class ProductFormTest < UnitTest
     assert_equal Product.last, product.product
   end
 
-  test "should create brand, root_category, category, subcategory and product if none exist" do
+  test 'should create brand, root_category, category, subcategory and product
+    if none exist' do
     product = @product
-    product.root_category = "Tessstt"
-    product.category = "Test"
-    product.subcategory = "Moar Test"
+    product.root_category = 'Tessstt'
+    product.category = 'Test'
+    product.subcategory = 'Moar Test'
     assert_empty Brand.where(name: product.brand.downcase)
     assert_empty Product.where(name: product.product_name.downcase)
-    assert_empty Category.where(name: product.category.downcase || product.subcategory.downcase || product.root_category.downcase)
+    assert_empty Category.where(name: product.category.downcase ||
+                                      product.subcategory.downcase ||
+                                      product.root_category.downcase)
 
     assert product.valid?
 
-    assert_differences [['Brand.count', 1], ['Product.count', 1], ['Category.count', 3]] do
+    assert_differences [['Brand.count', 1], ['Product.count', 1],
+                        ['Category.count', 3]] do
       product.save
     end
   end
 
-  test "should set user if user is given" do
+  test 'should set user if user is given' do
     product = @product
     product.user = users(:janik)
     assert product.valid?
