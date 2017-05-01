bb32bca9e4963dc805a37b0788370eb9f495f25f
Janik Knittle <janik.knittle@gmail.com>
2017-05-01 14:16:34 -0700

    Fix devise production url again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'thepartsninja.com' }
+  config.action_mailer.default_url_options = { host: 'www.thepartsninja.com' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
