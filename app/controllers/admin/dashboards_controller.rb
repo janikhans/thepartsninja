7c084e615842d932e349258ed95efe0e67592b44
Janik Knittle <janik.knittle@gmail.com>
2017-06-20 11:35:41 -0700

    Fix dashboard stats view

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,10 +1,9 @@
 class Admin::DashboardsController < Admin::ApplicationController
   def show
-    since_midnight = (Time.current.midnight - 24.hours)..Time.current.midnight
     last_24_hours = (Time.current - 24.hours)..Time.current
-    @recent_leads = Lead.where(created_at: since_midnight).count
-    @recent_searches = SearchRecord.where(created_at: since_midnight).count
-    @recent_users = User.where(created_at: since_midnight).count
+    @recent_leads = Lead.where('created_at::DATE = ?', Date.current).count
+    @recent_searches = SearchRecord.where('created_at::DATE = ?', Date.current).count
+    @recent_users = User.where('created_at::DATE = ?', Date.current).count
     @recent_forum_posts = ForumPost.where(created_at: last_24_hours)
                                    .includes(:user, forum_thread: :forum_topic)
                                    .page(params[:page])
